6bd6bab6f2d8114555d75e57c829964ea5465320
mariovyord <mariovyordanov@gmail.com>
2025-07-06 17:24:52 +0300

    First commit

---
+++ src/main/java/com/mariovyord/imdb_lite/ImdbLiteApplication.java
@@ -0,0 +1,13 @@
+package com.mariovyord.imdb_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariovyord/imdb_lite/ImdbLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariovyord.imdb_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

71fb2b4350b1105c8537536191bbbb9651eb01c6
mariovyord <mariovyordanov@gmail.com>
2025-07-07 16:35:55 +0300

    Add base repository

--- src/main/java/com/mariovyord/mdb_lite/repository/MovieRepository.java
+++ src/main/java/com/mariovyord/mdb_lite/repository/MovieRepository.java
@@ -1,8 +1,11 @@
 package com.mariovyord.mdb_lite.repository;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Repository;
 
+import com.mariovyord.mdb_lite.entity.MovieEntity;
+
 @Repository
-public interface MovieRepository {
-    
+public interface MovieRepository extends BaseRepository<MovieEntity, UUID> {
 } 
--- src/main/java/com/mariovyord/mdb_lite/service/impl/MovieServiceImpl.java
+++ src/main/java/com/mariovyord/mdb_lite/service/impl/MovieServiceImpl.java
@@ -1,7 +1,11 @@
 package com.mariovyord.mdb_lite.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.mariovyord.mdb_lite.entity.MovieEntity;
+import com.mariovyord.mdb_lite.repository.MovieRepository;
 import com.mariovyord.mdb_lite.service.MovieService;
 
 import de.dlh.lht.ti.model.MoviePageDto;
@@ -12,8 +16,13 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class MovieServiceImpl implements MovieService {
+    
+    private final MovieRepository movieRepository;
+
     @Override
     public MoviePageDto getMovies(MovieQueryParams movieQueryParams, MoviePagingCriteria pagingCriteria) {
+        List<MovieEntity> movies = movieRepository.findAll(); 
+        
         return null;
     }
 }

c42b7cb00e3f244cf7b2ccb1886c5c75ef28e144
mariovyord <mariovyordanov@gmail.com>
2025-07-10 17:26:48 +0300

    Add validation on create

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+[ ] Add GET Movies (sort, filter, pagination)
+[x] Add POST Movie
+[ ] Add Error Handling & RestExceptionHandler
+[ ] Add Keycloak
+[ ] Add GET Actors
+[ ] Add POST Actor
--- src/main/java/com/mariovyord/mdb_lite/service/impl/MovieServiceImpl.java
+++ src/main/java/com/mariovyord/mdb_lite/service/impl/MovieServiceImpl.java
@@ -14,6 +14,7 @@ import de.dlh.lht.ti.model.MoviePageDto;
 import de.dlh.lht.ti.model.MoviePagingCriteria;
 import de.dlh.lht.ti.model.MovieQueryParams;
 import lombok.AllArgsConstructor;
+import lombok.val;
 
 @Service
 @AllArgsConstructor
@@ -31,9 +32,16 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public MovieDto createMovie(MovieDto movieDto) {
+        validateDto(movieDto);
         MovieEntity movieEntity = movieMapper.toEntity(movieDto);
         movieEntity = movieRepository.save(movieEntity);
         
         return movieMapper.toDto(movieEntity);
+    }
+
+    private void validateDto(MovieDto movieDto) {
+        if (movieDto.getTitle() == null || movieDto.getTitle().trim().isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
     }
 }

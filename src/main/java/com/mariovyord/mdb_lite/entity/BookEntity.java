f8cadae0d5aff49418c5854199757227db445235
mariovyord <mariovyordanov@gmail.com>
2025-07-11 18:48:20 +0300

    Add author to book

--- src/main/java/com/mariovyord/mdb_lite/entity/BookEntity.java
+++ src/main/java/com/mariovyord/mdb_lite/entity/BookEntity.java
@@ -1,5 +1,6 @@
 package com.mariovyord.mdb_lite.entity;
 
+import java.util.Set;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -7,6 +8,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,4 +32,13 @@ public class BookEntity {
     
     @Column(name = "title", nullable = false)
     private String title;
+
+
+    @ManyToMany
+    @JoinTable(
+        name = "book_authors",
+        joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id")
+    )
+    private Set<AuthorEntity> authors;
 }
